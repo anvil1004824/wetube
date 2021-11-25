68a8dcb22197477bbbb9613ff828a94564749b35
anvil1004824 <anvil1004824@gmail.com>
2021-11-25 19:41:24 +0900

    User Authentication

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,14 +8,16 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  if (!video) return res.render("404", { pageTitle: "Video not found." });
+  if (!video)
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  if (!video) return res.render("404", { pageTitle: "Video not found." });
+  if (!video)
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
@@ -46,7 +48,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+  edit,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
+userRouter.route("/edit", edit);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
